7d33daaa160b9014b4b79a21769d7b9f0fe75103
buidat <dinhtuan95qh@gmail.com>
2022-06-15 23:48:52 +0700

    update

--- crud-nest/src/customer/customer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CustomerController } from './controller/customer/customer.controller';
-import { CustomerService } from './services/customer/customer.service';
-
-@Module({
-  controllers: [CustomerController],
-  providers: [CustomerService]
-})
-export class CustomerModule {}
